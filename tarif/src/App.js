761eb5d8a61b173279e1d6e0f8c875868bf8f378
Natalia <alt.antares@gmail.com>
2023-03-29 13:35:44 +0300

    added id to array elements

--- tarif/src/App.js
+++ tarif/src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 import Tarif from './components/TarifCard';
 
 const tars = [
-  {unlimited:"300", price: "300", speed: "10", color: 'blue'},
-  {unlimited:"450", price: "450", speed: "50", color: 'green'},
-  {unlimited:"550", price: "550", speed: "100", color: 'red', isSelected: true},
-  {unlimited:"1000", price: "1000", speed: "200", color: 'black'},
+  {unlimited:"300", price: "300", speed: "10", color: 'blue', id: 1},
+  {unlimited:"450", price: "450", speed: "50", color: 'green', id: 2},
+  {unlimited:"550", price: "550", speed: "100", color: 'red', id: 3, isSelected: true},
+  {unlimited:"1000", price: "1000", speed: "200", color: 'black', id: 4},
 ];
 
   function App() {
